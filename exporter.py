4649fd2d4195136a87ce39d4aadf640dee76a087
Rafael <rafaelbrandaobastos@gmail.com>
2024-12-02 01:46:28 -0300

    fix

--- exporter.py
+++ exporter.py
@@ -11,7 +11,10 @@ def gerar_dot(grafo_obj):
         # Estilização dos nós
         for i in range(len(grafo_obj.adj_matrix)):
             peso = grafo_obj.weights.get(grafo_obj.vertices[i], 0)
-            arquivo.write(f"    {i} [label=\"{grafo_obj.vertices[i]} (Peso: {peso})\", penwidth=2, shape=\"circle\"];\n")
+            if peso > 1:
+                arquivo.write(f"    {i} [label=\"{grafo_obj.vertices[i]} p:{peso}\", penwidth=2, shape=\"circle\"];\n")
+            else:
+                arquivo.write(f"    {i} [label=\"{grafo_obj.vertices[i]}\", penwidth=2, shape=\"circle\"];\n")
 
         # Estilização das arestas
         for i in range(len(grafo_obj.adj_matrix)):
